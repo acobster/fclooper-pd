bd824334246d3747e5af25557b2d5f6bee9e88b3
The Ambassador of Funk <acobster@gmail.com>
2017-06-13 21:43:43 -0700

    initial commit with four working buttons!

---
+++ README.md
@@ -0,0 +1,12 @@
+# TeensyDuino POC
+
+Looper proof-of-concept
+
+## Setup
+
+0. Open Arduino IDE
+1. Connect the board via USB
+1. Open Tools > Board, select Teensy 3.2 / 3.1
+2. Open Tools > USB Type, select Serial + MIDI
+3. Open Tools > Port, select the option that mentions "Teensy"
+4. Maybe run Teensy installer? IDK
---
+++ poc.ino
@@ -0,0 +1,107 @@
+#include <Bounce2.h>
+
+const int channel = 9;
+
+const int LED = 14;
+const int POT = 15;
+
+const int DEBOUNCE_INTERVAL = 55;
+
+int level;
+
+Bounce buttonC = Bounce();
+Bounce buttonD = Bounce();
+Bounce buttonE = Bounce();
+Bounce buttonF = Bounce();
+
+int val = 0;
+
+void setup() {
+  buttonC.attach(1);
+  buttonD.attach(2);
+  buttonE.attach(3);
+  buttonF.attach(4);
+
+  buttonC.interval(DEBOUNCE_INTERVAL);
+  buttonD.interval(DEBOUNCE_INTERVAL);
+  buttonE.interval(DEBOUNCE_INTERVAL);
+  buttonF.interval(DEBOUNCE_INTERVAL);
+
+  pinMode(LED, OUTPUT);
+  pinMode(POT, INPUT);
+
+  // Activate pullup resistors
+  pinMode(1, INPUT_PULLUP);
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
+
+  Serial.begin(38400); // open serial port
+  
+  // assign incoming MIDI note data to callback function
+  usbMIDI.setHandleNoteOn(OnNoteOn);
+  usbMIDI.setHandleNoteOff(OnNoteOff);
+  Serial.println("setup ran");
+}
+
+void loop() {
+  level = analogRead(POT);
+//  Serial.print("level at ");
+//  Serial.println(level);
+//  delay(250);
+
+  buttonC.update();
+  buttonD.update();
+  buttonE.update();
+  buttonF.update();
+
+  if (buttonC.fell()) {
+    usbMIDI.sendControlChange(1, 127, channel);
+    analogWrite(LED, HIGH); // 
+  }
+  if (buttonD.fell()) {
+    usbMIDI.sendControlChange(2, 127, channel);
+    analogWrite(LED, HIGH);
+  }
+  if (buttonE.fell()) {
+    usbMIDI.sendControlChange(3, 127, channel);
+  }
+  if (buttonF.fell()) {
+    usbMIDI.sendControlChange(4, 127, channel);
+  }
+
+  if (buttonC.rose()) {
+    usbMIDI.sendControlChange(1, 0, channel);
+    Serial.println("Played a C");
+    analogWrite(LED, LOW);
+  }
+  if (buttonD.rose()) {
+    usbMIDI.sendControlChange(2, 0, channel);
+    Serial.println("Played a D");
+    analogWrite(LED, LOW);
+  }
+  if (buttonE.rose()) {
+    usbMIDI.sendControlChange(3, 0, channel);
+    Serial.println("Played an E");
+  }
+  if (buttonF.rose()) {
+    usbMIDI.sendControlChange(4, 0, channel);
+    Serial.println("Played an F");
+  }
+
+  usbMIDI.read();
+  
+  val = analogRead(POT);
+}
+
+
+// note handling
+void OnNoteOn(byte channel, byte note, byte velocity) {
+  Serial.println("note on");
+}
+
+void OnNoteOff(byte channel, byte note, byte velocity) {
+  Serial.println("note off");
+}
+
+
